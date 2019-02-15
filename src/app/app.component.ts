33945cb9ac689c9d040391be2f58ce289351c093
Nikhil Jadhav <nikhil.jadhav1527@gmail.com>
2019-02-15 10:08:04 +0530

    Complete Code of Angular-Reddit Application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Article } from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,11 +7,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-reddit app';
+  
+  articles: Article[];
+
+  constructor(){
+    this.articles = [
+      new Article('Angular 7', 'https://www.angular.io', 5),
+      new Article('Facebook', 'https://www.facebook.com', 8),
+      new Article('Google', 'https://www.google.com', 10)
+    ];
+  }
 
   addArticle(title: HTMLInputElement, link:HTMLInputElement) {
     console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value ='';
+    link.value='';
     return false;
+  }
+
+
+  sortedArticles(): Article[]{
+    return this.articles.sort((a:Article, b:Article) => b.votes - a.votes);
   }
 
 }
--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, HostBinding } from '@angular/core';
+import { Component, OnInit, HostBinding, Input } from '@angular/core';
 import { Article } from './article.model';
 
 @Component({
@@ -9,15 +9,9 @@ import { Article } from './article.model';
 export class ArticleComponent implements OnInit {
 
   @HostBinding('attr.class') cssClass = 'row';
-  article: Article;
+  @Input() article: Article;
 
-  constructor() {
-    this.article = new Article(
-      'Angular 7',
-      'https://www.angular.io',
-      10
-    );
-  }
+  constructor() { }
 
   ngOnInit() {
   }
